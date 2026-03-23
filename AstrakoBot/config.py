d09dfec91451f27780f7ca74a687c14006d70228
ᯓ‌𐌌།⃪᱂ ⚠️𝑻𝒉𝒂𝒌𝒖𝒓𓂃 <reetsingh274@gmail.com>
2026-03-24 03:29:19 +0530

    Update config.py

--- AstrakoBot/config.py
+++ AstrakoBot/config.py
@@ -3,7 +3,7 @@ import os
 class Development:
     OWNER_ID = int(os.environ.get("OWNER_ID", "0"))
     OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "")
-    API_KEY = os.environ.get("API_KEY", "")
+    TOKEN = os.environ.get("API_KEY", "")
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI", "")
     JOIN_LOGGER = os.environ.get("JOIN_LOGGER", "")
     USE_JOIN_LOGGER = True
