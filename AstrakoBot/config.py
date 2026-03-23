435553d70163fff694e10114ceab5c5d1bd912c9
ᯓ‌𐌌།⃪᱂ ⚠️𝑻𝒉𝒂𝒌𝒖𝒓𓂃 <reetsingh274@gmail.com>
2026-03-24 03:39:10 +0530

    Update config.py

--- AstrakoBot/config.py
+++ AstrakoBot/config.py
@@ -4,12 +4,18 @@ class Development:
     OWNER_ID = int(os.environ.get("OWNER_ID", "0"))
     OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "")
     TOKEN = os.environ.get("API_KEY", "")
+
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI", "")
+
     JOIN_LOGGER = os.environ.get("JOIN_LOGGER", "")
     USE_JOIN_LOGGER = True
+
     ALLOW_CHATS = True
+
     DEV_USERS = []
     SUPPORT_USERS = []
     SUDO_USERS = []
+    WHITELIST_USERS = []
+
     LOAD = []
     NO_LOAD = []
